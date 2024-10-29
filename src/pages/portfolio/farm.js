21804bd7d683ce9e649a5f30fed5b8bd1939be95
na0213 <natsumikan0213@gmail.com>
2024-10-30 03:40:17 +0900

    1030

--- src/pages/portfolio/farm.js
+++ src/pages/portfolio/farm.js
@@ -15,22 +15,50 @@ const Farm = () => {
 
       <div className={styles.farm}>
         <div className={styles.farm__title}>オリジナルWEBサイトーFarm360ー</div>
-        <Link href="http://www.farm360.jp/" target="_blank" rel="noopener noreferrer">
-
-            <p className={styles.farm__pr}>巡った牧場をご紹介できるWEBサイトを制作。<br>
-            </br>インタビュー記事や撮影画像などを掲載し牧場の魅力を伝えていきます。<br>
-            </br>▶︎WEBサイトへ</p>
-            <div className={styles.farm__imagecontainer}>
+        <div onClick={handleModalOpen} className={styles.farm__link}>
+          <p className={styles.farm__pr}>
+            初めて開発したサービス。<br />
+            出店されている食品の原材料を指定し除外して検索できるサービスを開発。<br />
+            Laravel・PHPを使用しています。<br />
+            ▶︎WEBサイトへ
+          </p>
+          
+          <div className={styles.farm__imagecontainer}>
             <Image
-                  src="/farmpic.png" // publicフォルダにある画像へのパス
-                  alt="Farm Image"
-                  width={1200} // 画像の幅
-                  height={400} // 画像の高さ
-                  layout="responsive" // 画像をレスポンシブに表示
-                  className={styles.farm__image}
+              src="/farmpic.png" // publicフォルダにある画像へのパス
+              alt="FOOCH Image"
+              width={1200}
+              height={400}
+              layout="responsive"
+              className={styles.farm__image}
             />
-      </div>
-        </Link>
+          </div>
+        </div>
+        {/* モーダルウィンドウ */}
+        {isModalOpen && (
+          <div className={styles.modal}>
+            <div className={styles.modalContent}>
+              <p>11月中に公開予定です🐧</p>
+              <button onClick={handleModalClose}>閉じる</button>
+            </div>
+          </div>
+        )}
+
+        {/* <Link href="http://www.farm360.jp/" target="_blank" rel="noopener noreferrer">
+          <p className={styles.farm__pr}>巡った牧場をご紹介できるWEBサイトを制作。<br>
+          </br>インタビュー記事や撮影画像などを掲載し牧場の魅力を伝えていきます。<br>
+          </br>▶︎WEBサイトへ</p>
+          <div className={styles.farm__imagecontainer}>
+            <Image
+              src="/farmpic.png" // publicフォルダにある画像へのパス
+              alt="Farm Image"
+              width={1200} // 画像の幅
+              height={400} // 画像の高さ
+              layout="responsive" // 画像をレスポンシブに表示
+              className={styles.farm__image}
+            />
+          </div>
+        </Link> */}
       </div>
       {/* 戻る */}
       <div className={styles.back}>
