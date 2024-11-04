053890589e947d991aad721a5be3eb3196374af2
na0213 <natsumikan0213@gmail.com>
2024-11-05 03:41:06 +0900

    1105-2

--- src/pages/portfolio/farm.js
+++ src/pages/portfolio/farm.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from "@/styles/Home.module.css";
